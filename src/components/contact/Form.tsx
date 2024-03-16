edc1a75e5a8e582964c1536432855c7e04f5b936
Ezewi Osorachukwu <oezewi@gmail.com>
2024-03-16 13:23:16 +0100

    retraying my second commit

--- src/components/contact/Form.tsx
+++ src/components/contact/Form.tsx
@@ -1,68 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./form.module.css";
 
 const Form = () => {
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { firstName, lastName, email, phoneNumber, message } = formData;
+    const mailtoLink = mailto:christinotochukwu@gmail.com?subject=Contact Form Submission&body=First Name: ${firstName}%0D%0ALast Name: ${lastName}%0D%0AEmail: ${email}%0D%0APhone Number: ${phoneNumber}%0D%0AMessage: ${message};
+
+    window.open(mailtoLink);
+  };
+
   return (
     <form className="lg:w-1/2 w-full">
       <div className="sm:flex sm:justify-center sm:gap-2 flex-col">
-        {/* <div>
-          <input
-            type="text"
-            placeholder="First Name"
-            className="pl-2 outline-none border-b-2 h-20 w-full"
-          />
-          <input
-            type="text"
-            placeholder="Email Address"
-            className="pl-2 outline-none border-b-2 h-20 w-full"
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Last Name"
-            className="pl-2 outline-none border-b-2 h-20 w-full"
-          />
-          <input
-            type="text"
-            placeholder="Phone Number"
-            className="pl-2 outline-none border-b-2 h-20 w-full"
-          />
-        </div>
-        <div className="">
-          <textarea
-            name=""
-            id=""
-            cols={5}
-            rows={2}
-            placeholder="Message"
-            className="mx-auto w-full border-b border-black outline-none sm:mt-2"
-          ></textarea>
-        </div> */}
+       
         <div className="flex gap-2 w-full">
           <input
             type="text"
             placeholder="First Name"
+            name="firstName"
+            value={formData.firstName} 
+            onChange={handleChange}
             className="pl-2 outline-none border-b-2 h-20 w-1/2"
           />
           <input
             type="text"
             placeholder="Email Address"
+            name="email" 
+            value={formData.email} 
+            onChange={handleChange}
             className="pl-2 outline-none border-b-2 h-20 w-1/2"
           />
         </div>
+
         <div className="flex gap-2 w-full">
           <input
             type="text"
             placeholder="Last Name"
+            name="lastName" 
+            value={formData.lastName} 
+            onChange={handleChange}
             className="pl-2 outline-none border-b-2 h-20 w-full"
           />
           <input
             type="text"
             placeholder="Phone Number"
+            name="phoneNumber" 
+            value={formData.phoneNumber} 
+            onChange={handleChange}
             className="pl-2 outline-none border-b-2 h-20 w-full"
           />
         </div>
+
         <div className="w-full">
           <textarea
             name=""
@@ -70,6 +78,9 @@ const Form = () => {
             cols={5}
             rows={2}
             placeholder="Message"
+            name="message" 
+            value={formData.message} 
+            onChange={handleChange}
             className="mx-auto w-full pl-2 border-b border-black outline-none sm:mt-2"
           ></textarea>
         </div>
